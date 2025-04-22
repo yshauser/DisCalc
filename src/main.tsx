ceae15a85aa397f0dc82c626716b86eafd353a5a
yshauser <yshauser@gmail.com>
2025-04-22 19:06:03 +0300

    updates for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './main.css';
 import App from './App';
 
 console.log ('in main');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
