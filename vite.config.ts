5cb33ad37d2020948b3f5a76e18af96407b634fa
yshauser <yshauser@gmail.com>
2025-04-22 19:09:01 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/DisCalc/',
   plugins: [react()],
   server: {
     port: 5174, // change if you want a different port
